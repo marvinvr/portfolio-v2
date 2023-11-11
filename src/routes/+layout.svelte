11ec5f3360d35e497e2dec4f5fbd6706e0301030
Marvin von Rappard <marvin@von-rappard.ch>
2023-11-11 14:07:26 +0100

    feat: add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
-    import Footer from "$lib/components/Footer.svelte";
-import "../app.css";
-  </script>
-  
-  <slot />
-  <Footer />
+  import Footer from "$lib/components/Footer.svelte";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? 'development' : 'production' });
+
+
+  import "../app.css";
+</script>
+
+<slot />
+<Footer />
