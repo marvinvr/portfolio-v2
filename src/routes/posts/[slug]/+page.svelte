fa94d1e4c2816b6021853e5eb08e10f2ad664528
Marvin von Rappard <marvin@von-rappard.ch>
2023-11-23 09:35:05 +0100

    Update +page.svelte

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -21,8 +21,8 @@
 <svelte:head>
   <title>{data.meta.title} | Marvin von Rappard</title>
 
-  <link rel="canonical" href="https://marvinvr.ai/posts/{slug}/" />
-  <meta property="og:url" content="https://marvinvr.ai/posts/{slug}/" />
+  <link rel="canonical" href="https://marvinvr.ai/posts/{slug}" />
+  <meta property="og:url" content="https://marvinvr.ai/posts/{slug}" />
 
   <meta property="og:type" content="article" />
   <meta property="og:title" content={meta.title} />
