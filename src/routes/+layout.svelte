2f8c6cabdef8d4e3269ef04b245696f6767da859
Marvin von Rappard <marvin@von-rappard.ch>
2024-06-13 12:17:25 +0200

    bun fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script>
     import Footer from "$lib/components/Footer.svelte";
     import {dev} from "$app/environment";
-    import {inject} from "@vercel/analytics";
-    import {injectSpeedInsights} from '@vercel/speed-insights/sveltekit';
-
-    inject({mode: dev ? 'development' : 'production'});
-
-    injectSpeedInsights();
 
     import "../app.scss";
 </script>
