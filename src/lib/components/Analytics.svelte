a3643367d1002a8c9c65d45e7481a7053c9927df
Marvin von Rappard <marvin@von-rappard.ch>
2025-04-12 14:55:54 +0200

    fix: update analytics script to load only in production environment

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { PUBLIC_CF_ANALYTICS_ID } from "$env/static/public";
-
-  const src = `https://static.cloudflareinsights.com/beacon.min.js?token=${PUBLIC_CF_ANALYTICS_ID}`;
+  import { PUBLIC_ENVIRONMENT } from "$env/static/public";
 </script>
 
 <svelte:head>
-  <script defer {src}></script>
+  {#if PUBLIC_ENVIRONMENT === "production"}
+    <script defer data-domain="marvinvr.ch" src="https://stats.mvr.bz/js/script.js"></script>
+  {/if}
 </svelte:head>
