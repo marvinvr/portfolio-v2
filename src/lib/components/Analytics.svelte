068196d6d544d007630ef8d78523a0afe031ee18
Marvin von Rappard <marvin@von-rappard.ch>
2025-05-18 20:10:50 +0200

    fix: add conditional loading for analytics script in production environment

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <svelte:head>
+  
   {#if PUBLIC_ENVIRONMENT === "production"}
     <script defer data-domain="marvinvr.ch" src="https://stats.marvinvr.ch/js/script.js"></script>
   {/if}
+
 </svelte:head>
