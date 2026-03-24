8847a7d08244d91cce2675e821183f1840ba42c5
Marvin von Rappard <marvin@von-rappard.ch>
2026-03-24 17:17:59 +0100

    Fix Tracking

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -3,9 +3,12 @@
 </script>
 
 <svelte:head>
-  
   {#if PUBLIC_ENVIRONMENT === "production"}
     <script defer data-domain="marvinvr.ch" src="https://stats.marvinvr.ch/js/script.js"></script>
+    <script
+      src="https://rybbit.marvinvr.ch/api/script.js"
+      data-site-id="008653442f42"
+      defer
+    ></script>
   {/if}
-
 </svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import Footer from "$lib/components/Footer.svelte";
+  import Analytics from "$lib/components/Analytics.svelte";
 
   import "../app.scss";
-  import Analytics from "$lib/components/Analytics.svelte";
-  import { onMount } from "svelte";
+
   interface Props {
     children?: import('svelte').Snippet;
   }
